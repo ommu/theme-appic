6f194281e58c054af0ac1f93f77e85f358ec9afd
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-12-26 22:53:21 +0700

    add template controllers

---
+++ controllers/TemplateController.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * TemplateController
+ * @var $this TemplateController
+ * version: 0.0.1
+ * Reference start
+ *
+ * TOC :
+ *	Index
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2017 Ommu Platform (opensource.ommu.co)
+ * @link https://github.com/OmmuThemes/appic
+ *
+ *----------------------------------------------------------------------------------------------------------
+ */
+
+class TemplateController extends Controller
+{
+	/**
+	 * Initialize public template
+	 */
+	public function init() 
+	{
+		$arrThemes = Utility::getCurrentTemplate('public');
+		Yii::app()->theme = $arrThemes['folder'];
+		$this->layout = $arrThemes['layout'];
+		Utility::applyViewPath(__dir__, false);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules() 
+	{
+		return array(
+			array('allow',  // allow all users to perform 'index' and 'view' actions
+				'actions'=>array('index','static1','static2','animated','slider1','slider2','video'),
+				'users'=>array('*'),
+			),
+		);
+	}
+	
+	/**
+	 * Displays the login page
+	 */
+	public function actionStatic1()
+	{
+		$this->pageTitle = Yii::t('phrase', 'Static Image 1');
+		$this->pageDescription = '';
+		$this->pageMeta = '';
+		$this->render('front_static_1');
+	}
+	
+	/**
+	 * Displays the login page
+	 */
+	public function actionStatic2()
+	{
+		$this->pageTitle = Yii::t('phrase', 'Static Image 2');
+		$this->pageDescription = '';
+		$this->pageMeta = '';
+		$this->render('front_static_2');
+	}
+	
+	/**
+	 * Displays the login page
+	 */
+	public function actionAnimated()
+	{
+		$this->pageTitle = Yii::t('phrase', 'Animated Headlines');
+		$this->pageDescription = '';
+		$this->pageMeta = '';
+		$this->render('front_animated');
+	}
+	
+	/**
+	 * Displays the login page
+	 */
+	public function actionSlider1()
+	{
+		$this->pageTitle = Yii::t('phrase', 'Slider 1');
+		$this->pageDescription = '';
+		$this->pageMeta = '';
+		$this->render('front_slider_1');
+	}
+	
+	/**
+	 * Displays the login page
+	 */
+	public function actionSlider2()
+	{
+		$this->pageTitle = Yii::t('phrase', 'Slider 2');
+		$this->pageDescription = '';
+		$this->pageMeta = '';
+		$this->render('front_slider_2');
+	}
+	
+	/**
+	 * Displays the login page
+	 */
+	public function actionVideo()
+	{
+		$this->pageTitle = Yii::t('phrase', 'Video Background');
+		$this->pageDescription = '';
+		$this->pageMeta = '';
+		$this->render('front_video');
+	}
+}
---
+++ views/template/front_static_2.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @var $this TemplateController
+ * @var $error array
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2017 Ommu Platform (opensource.ommu.co)
+ * @link https://github.com/OmmuThemes/appic
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Main',
+	);
+?>
