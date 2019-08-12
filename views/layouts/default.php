7919cae2eda22d02a865d5c7849274478d761602
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-08-12 19:07:42 +0700

    modify author + copyright

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @author Putra Sudaryanto <putra@ommu.co>
  * @contact (+62)856-299-4114
- * @copyright Copyright (c) 2017 Ommu Platform (opensource.ommu.co)
+ * @copyright Copyright (c) 2017 Ommu Platform (www.ommu.co)
  * @created date 26 December 2017, 20:45 WIB
  * @link https://github.com/OmmuThemes/appic
  *
